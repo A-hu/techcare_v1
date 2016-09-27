7e8bdbf728ffa7aadebdb2f45b54e56de91760b0
A-hu <louis0911317399@gmail.com>
2016-09-27 19:43:23 +0800

    Add roles page & caregiver controller and views

---
+++ app/controllers/caregivers_controller.rb
@@ -0,0 +1,45 @@
+class CaregiversController < ApplicationController
+
+	before_action :find_caregiver, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@caregivers = Caregiver.all
+	end
+
+	def show
+	end
+
+	def new
+		@caregiver = Caregiver.new
+	end
+
+	def create
+		@caregiver = Caregiver.new( set_params )
+		@caregiver.user = current_user
+		@caregiver.save
+		redirect_to caregivers_path
+	end
+
+	def edit
+	end
+
+	def update
+		@caregiver.update( set_params )
+		redirect_to caregivers_path
+	end
+
+	def destroy
+		@caregiver.destroy
+		redirect_to caregivers_path
+	end
+
+	private
+
+	def find_caregiver
+		@caregiver = Caregiver.find(params[:id])
+	end
+
+	def set_params
+		params.require(:caregiver).permit(:user_id, :introduction, :experience, :skill, :licence_number, :licence_offdate, :training)
+	end
+end
---
+++ spec/controllers/caregivers_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe CaregiversController, type: :controller do
+
+end
