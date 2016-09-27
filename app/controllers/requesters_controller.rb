96740b831eda4e616a623dd510ce214bee82cfae
A-hu <louis0911317399@gmail.com>
2016-09-27 22:27:14 +0800

    Add requester controller and views

---
+++ app/controllers/requesters_controller.rb
@@ -0,0 +1,45 @@
+class RequestersController < ApplicationController
+
+	before_action :find_requester, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@requesters = Requester.all
+	end
+
+	def show
+	end
+
+	def new
+		@requester = Requester.new
+	end
+
+	def create
+		@requester = Requester.new ( set_params )
+		@requester.user = current_user
+		@requester.save
+		redirect_to requesters_path
+	end
+
+	def edit
+	end
+
+	def update
+		@requester.update ( set_params )
+		redirect_to requesters_path
+	end
+
+	def destroy
+		@requester.destroy
+		redirect_to requesters_path
+	end
+
+	private
+
+	def find_requester
+		@requester = Requester.find(params[:id])
+	end
+
+	def set_params
+		params.require(:requester).permit(:user_id, :address, :condition_description)
+	end
+end
