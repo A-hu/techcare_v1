354d4e6bdeb945b034f97094a427e015c0c8190f
A-hu <louis0911317399@gmail.com>
2016-10-02 16:14:21 +0800

    rspec for user, caregiver and requester

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryGirl.define do
   factory :user do
-    
+  	first_name "Edward"
+  	last_name "Lee"
+  	nickname "EDW"
+  	cell_phone_number "0912345678"
+  	email "Edward@gmail.com"
+  	password "123123"
   end
 end
