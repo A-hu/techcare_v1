701b502ed173146ad2066b3c975156a7cf1a3a7f
A-hu <louis0911317399@gmail.com>
2016-10-12 00:25:45 +0800

    hot fix migration

--- db/migrate/20161009131951_create_comment_categories.rb
+++ db/migrate/20161009131951_create_comment_categories.rb
@@ -1,11 +1,11 @@
 class CreateCommentCategories < ActiveRecord::Migration[5.0]
-  def change
-    create_table :comment_categories do |t|
-    	t.string :name
+  # def change
+  #   create_table :comment_categories do |t|
+  #   	t.string :name
 
-      t.timestamps
-    end
-    add_column :comments, :comment_category_id, :integer
-    add_index  :comments, :comment_category_id
-  end
+  #     t.timestamps
+  #   end
+  #   add_column :comments, :comment_category_id, :integer
+  #   add_index  :comments, :comment_category_id
+  # end
 end
