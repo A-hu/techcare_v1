bec98ed98bd4a084a78ba1946a242dca9fe65656
A-hu <louis0911317399@gmail.com>
2016-10-13 16:41:38 +0800

    Update api itemsList

--- app/controllers/api_v1/items_controller.rb
+++ app/controllers/api_v1/items_controller.rb
@@ -3,10 +3,13 @@ class ApiV1::ItemsController < ApiController
   before_action :authenticate_user!
 
 	def show
-		if schedule = current_user.caregiver.schedules.find_by_scheduled_date(params[:care_date]) && requester = Requester.find(params[:requester_id])
-			events = schedule.events.where(:requester_id => requester.id)
+		if schedule = current_user.caregiver.schedules.find_by( scheduled_date: params[:care_date].to_date, requester_id: params[:requester_id] )
+			@events = schedule.events
+		elsif params[:requester_id] == ""
+			schedules = current_user.caregiver.schedules.where( scheduled_date: params[:care_date].to_date )
+			@events = schedules.map{ |s| s.events }.flatten
 		else
-			render json: { message: "Fail"}, status: 400
+			render json: { message: "Fail" }, status: 400
 		end
 	end
 
@@ -15,10 +18,10 @@ class ApiV1::ItemsController < ApiController
 		schedule = current_user.caregiver.schedules.find_by( scheduled_date: params[:care_date].to_date, requester_id: params[:requester_id] )
 
 		if schedule.present?
-			has_fails = create_event(schedule, params[:items_data])
+			has_fails = create_event( schedule, params[:items_data] )
 		else
 			schedule = current_user.caregiver.schedules.create( scheduled_date: params[:care_date].to_date, requester_id: params[:requester_id] )
-			has_fails = create_event(schedule, params[:items_data])
+			has_fails = create_event( schedule, params[:items_data] )
 		end
 
 		has_fails == [] ? ( render json: { status: "200", message: "OK" }, status: 200 ) : ( render json: { status: "400", message: "Fail", scheduled_date: schedule.scheduled_date, fails: has_fails }, status: 400 )
@@ -29,9 +32,9 @@ class ApiV1::ItemsController < ApiController
 
 		schedule = current_user.caregiver.schedules.find_by( scheduled_date: params[:care_date].to_date, requester_id: params[:requester_id] )
 		data = JSON.parse( params[:item_data] )
-		event = schedule.events.find_by_id(data["item_id"])
+		event = schedule.events.find_by_id( data["item_id"] )
 
-		event.presents ? ( render json: { status: "200", message: "OK" }, status: 200 ) : ( render json: { status: "400", message: "This event is not existing."}, status: 400 )
+		event.presents ? ( render json: { status: "200", message: "OK" }, status: 200 ) : ( render json: { status: "400", message: "This event is not existing." }, status: 400 )
 
 	end
 
@@ -49,11 +52,11 @@ class ApiV1::ItemsController < ApiController
 		has_fails = []
 		data = JSON.parse( raw_data )
 		data["items_data"].each do |item|
-			t = TimeZone.find_by_zone(item["operation_time"])
+			t = TimeZone.find_by_zone( item["operation_time"] )
 			if schedule.events.find_by_time_zone_id(t.id).present?
 				has_fails << "#{item["operation_time"]} is already booked."
 			else
-				schedule.events.create( push: item["important"], time_zone_id: t.id)
+				schedule.events.create( push: item["important"], time_zone_id: t.id )
 			end
 		end
 		has_fails
--- app/views/api_v1/items/show.json.jbuilder
+++ app/views/api_v1/items/show.json.jbuilder
@@ -1,10 +1,12 @@
 json.status "200"
 json.message "OK"
 json.items_data @events do |e|
-	json.event_id e.id
+	json.requester_name e.schedule.requester.user.first_name + e.schedule.requester.user.first_name
+	json.item_id e.id
 	json.operation_time e.time_zone.zone
-	json.name e.demands.map {|d| d.demand_name}
+	json.name e.demands.map { |d| d.demand_name }
 	json.important e.push
+	json.complete_time e.complete_time
 end
 
       # "event_id" : "18274",
