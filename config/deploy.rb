7a8229d0c5f1f28502887fe2208bb0ed02477c52
A-hu <louis0911317399@gmail.com>
2016-10-14 21:26:31 +0800

    hot fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, 'git@github.com:A-hu/techcare_v1.git'
 set :deploy_to, '/home/deploy/techcare'
 set :keep_releases, 5
 
-append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_files, 'config/database.yml', 'config/secrets.yml', 'config/email.yml'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 set :passenger_restart_with_touch, true
 # Default branch is :master
