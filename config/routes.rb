6dd8a0efb7374fea7985d687d1a13124a92d3e80
A-hu <louis0911317399@gmail.com>
2016-10-15 09:17:04 +0800

    Add welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'schedules/:requester_id/:date', :to => 'schedules#recent_days', :as => "schedules/recent_days"
   resources :health_records
   resources :comments
-  root "schedules#index"
+  root "welcomes#index"
 
   scope :path => '/api/v1/', :module => "api_v1", :defaults => { :format => :json }, :as => 'v1' do
 
