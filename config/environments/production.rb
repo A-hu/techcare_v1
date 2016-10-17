092599250c0f2cf277ec75f4794c155dc78bd0aa
A-hu <louis0911317399@gmail.com>
2016-10-17 11:43:14 +0800

    hot fix for UserMailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "techcare_v1_#{Rails.env}"
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+  config.action_mailer.default_url_options = { host: "http://103.3.61.202" }
   config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
 
   # Ignore bad email addresses and do not raise email delivery errors.
