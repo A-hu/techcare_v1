7c7637b0822bff1d5f7d1f21381bca7f702c9167
A-hu <louis0911317399@gmail.com>
2016-10-18 22:02:55 +0800

    Add and Remove medication picture and associate to schedules

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -8,15 +8,18 @@ class MedicationsController < ApplicationController
 	end
 
 	def create
+		date = Time.now
 		@medication = @requester.medications.new( set_params )
 		@medication.save
-		event_create( @medication )
+		event_create( @medication, date ) unless @medication.medication_time.id == 1
 		redirect_to requester_medications_path( @requester )
 	end
 
 	def destroy
+		date = Time.now.to_date
 		@medication = Medication.find( params[:id] )
 		@medication.destroy
+		remove_demand( @requester, @medication, date )
 		redirect_to requester_medications_path( @requester )
 	end
 
@@ -31,7 +34,46 @@ class MedicationsController < ApplicationController
 	end
 
 	def event_create( medication, date )
-		Event.create()medication.medication_time
+
+		i = 0
+		until (date + i.days).wday == 0
+			schedule = Schedule.find_by( scheduled_date: (date + i.days).to_date )
+			if schedule.present?
+				t = TimeZone.find_by_zone( medication.medication_time.take_time )
+				event = schedule.events.find_by( time_zone_id: t.id )
+				if event.present?
+					event.demands << Demand.find_by(demand_name: medication.medication_time.name)
+					event.time_zone = TimeZone.find_by_zone( medication.medication_time.take_time )
+				else
+					event = schedule.events.create unless medication.medication_time.id == 1
+					event.demands << Demand.find_by(demand_name: medication.medication_time.name)
+					event.time_zone = TimeZone.find_by_zone( medication.medication_time.take_time )
+				end
+			else
+				schedule = Schedule.create( scheduled_date: (date + i.days).to_date )
+				event = schedule.events.create unless medication.medication_time.id == 1
+				event.demands << Demand.find_by(demand_name: medication.medication_time.name)
+				event.time_zone = TimeZone.find_by_zone( medication.medication_time.take_time )
+			end
+			event.save
+			i += 1
+		end
+		
+	end
+
+	def remove_demand( requester, medication, date )
+		
+		i = 0
+		until (date + i.days).wday == 0
+			schedule = requester.schedules.find_by( scheduled_date: (date + i.days).to_date )
+			t = TimeZone.find_by_zone( medication.medication_time.take_time )
+			event = schedule.events.find_by( time_zone_id: t.id )
+			medication_demand = medication.medication_time.name
+			demand = Demand.find_by( demand_name: medication_demand )
+			event.demands.destroy(demand)
+			event.destroy if event.demands == []
+			i += 1
+		end
 	end
 
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,8 +1,8 @@
 class Schedule < ApplicationRecord
-	validates_presence_of :caregiver_id, :scheduled_date
+	validates_presence_of :scheduled_date
 	validates :scheduled_date, uniqueness: { scope: :requester_id }
-	belongs_to :caregiver
-	belongs_to :requester
+	belongs_to :caregiver, optional: true
+	belongs_to :requester, optional:true
 	has_many :events, dependent: :destroy
 
 	has_many :comments, as: :commentable, dependent: :destroy
